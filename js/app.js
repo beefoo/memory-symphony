1f2c1019f5d9ed77f0558212c6dd0b7f5a5a6634
Brian Foo <brian@youaremyjoy.org>
2016-06-01 17:10:48 -0400

    basic pitch and volume feedback via microphone

---
+++ js/app.js
@@ -0,0 +1,216 @@
+var App = (function() {
+  function App(options) {
+    var defaults = {
+      correlationMin: 0.9,
+      fftsize: 2048,
+      // fundamental frequency of speech can vary from 40 Hz for low-pitched male voices
+      // to 600 Hz for children or high-pitched female voices
+      // https://en.wikipedia.org/wiki/Pitch_detection_algorithm#Fundamental_frequency_of_speech
+      frequencyMin: 40,
+      frequencyMax: 600,
+      minRms: 0.01, // min signal
+      phraseDurationMin: 10 // in milliseconds
+    };
+    this.opt = _.extend({}, defaults, options);
+    this.init();
+  }
+
+  App.prototype.init = function(){
+    // init audio context
+    var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+    this.ctx = new AudioContext();
+    this.sampleRate = this.ctx.sampleRate;
+    this.listenForMicrophone();
+  };
+
+  // Autocorrelation algorithm snagged from: https://github.com/cwilso/PitchDetect
+  App.prototype.autoCorrelate = function(buf){
+    var bufLen = buf.length,
+        periods = this.periods,
+        periodLen = periods.length,
+        maxSamples = this.maxSamples,
+        sampleRate = this.sampleRate,
+        minCorrelation = this.opt.correlationMin;
+
+    // check to see if enough signal
+    var rms = this._rootMeanSquare(buf);
+    if (rms < this.opt.minRms) return -1;
+
+    var pitch = -1;
+    var best_offset = -1;
+    var best_correlation = 0;
+    var foundGoodCorrelation = false;
+    var correlations = new Array(maxSamples);
+
+    var lastCorrelation=1;
+
+    for (i=0; i<periodLen; i++) {
+      var offset = periods[i];
+      var correlation = 0;
+      for (var j=0; j<maxSamples; j++) {
+        correlation += Math.abs((buf[j])-(buf[j+offset]));
+      }
+      correlation = 1 - (correlation/maxSamples);
+      correlations[offset] = correlation;
+      if ((correlation > minCorrelation) && (correlation > lastCorrelation)) {
+        foundGoodCorrelation = true;
+        if (correlation > best_correlation) {
+          best_correlation = correlation;
+          best_offset = offset;
+        }
+
+      } else if (foundGoodCorrelation) {
+        var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];
+        pitch = sampleRate/(best_offset+(8*shift));
+        break;
+      }
+      lastCorrelation = correlation;
+    }
+    if (best_correlation > 0.01) {
+      // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+      pitch = sampleRate/best_offset;
+    }
+
+    return pitch;
+  };
+
+  App.prototype.listen = function(){
+    // stop listening
+    if (!this.listening) {
+      if (this.lastTime) this.endTime = this.lastTime;
+      return false;
+    }
+
+    // keep track of time
+    var now = new Date();
+    var timeSince = 0;
+    if (this.lastTime) timeSince = now - this.lastTime;
+    else this.startTime = now;
+    this.lastTime = now;
+
+    // put frequency data into buffer
+    var buffer = new Float32Array(this.bufferLen);
+    this.analyzer.getFloatTimeDomainData(buffer);
+
+    // retrieve pitch via autocorrelation
+    var pitch = this.autoCorrelate(buffer);
+    var volume = this._rootMeanSquare(buffer);
+    // this.pitch_buffer.push(pitch);
+
+    this.render(pitch, volume);
+
+    // continue to listen
+    requestAnimationFrame(this.listen.bind(this));
+    // var _this = this;
+    // setTimeout(function(){_this.listen();}, 2000);
+  };
+
+  App.prototype.listenForMicrophone = function(){
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    navigator.getUserMedia({audio: true},
+      this.onStream.bind(this),
+      this.onStreamError.bind(this));
+  };
+
+  App.prototype.listenOn = function(){
+    // reset times
+    this.lastTime = false;
+    this.endTime = false;
+
+    // start listening
+    this.listening = true;
+    this.listen();
+  };
+
+  App.prototype.listenOff = function(){
+    this.listening = false;
+  };
+
+  // Setup analyzer after microphone stream is initialized
+  App.prototype.onStream = function(stream){
+    var input = this.ctx.createMediaStreamSource(stream);
+    var analyzer = this.ctx.createAnalyser();
+
+    analyzer.smoothingTimeConstant = 0;
+    analyzer.fftSize = this.opt.fftsize;
+
+    // Connect graph
+    input.connect(analyzer);
+    this.analyzer = analyzer;
+    this.bufferLen = this.analyzer.frequencyBinCount; // should be 1/2 fftSize
+    this.maxSamples = Math.floor(this.bufferLen/2);
+    this.updatePeriods();
+
+    // init pitch buffer
+    this.pitch_buffer = [];
+
+    // And listen
+    this.listenOn();
+  };
+
+  App.prototype.onStreamError = function(e){
+    console.log(e);
+    alert('Error: '+e.name+' (code '+e.code+')');
+  };
+
+  App.prototype.render = function(pitch, volume){
+    this.$el = this.$el || $('#debug');
+
+    if (pitch > 0) {
+      this.$el.text(pitch);
+      this.$el.css('font-size', (volume*100)+'em');
+    } else {
+      this.$el.text('');
+    }
+
+  };
+
+  App.prototype.updatePeriods = function(){
+    // Determine min/max period
+    var minPeriod = this.opt.minPeriod || 2;
+    var maxPeriod = this.opt.maxPeriod || this.maxSamples;
+    if(this.opt.frequencyMin) maxPeriod = Math.floor(this.sampleRate / this.opt.frequencyMin);
+    if(this.opt.frequencyMax) minPeriod = Math.ceil(this.sampleRate / this.opt.frequencyMax);
+    maxPeriod = Math.min(maxPeriod, this.maxSamples);
+    minPeriod = Math.max(2, minPeriod);
+
+    // init periods
+    this.periods = [];
+    for(var i = minPeriod; i <= maxPeriod; i++) {
+      this.periods.push(i);
+    }
+  };
+
+  App.prototype._mean = function(arr) {
+    var sum = 0,
+        arrLen = arr.length;
+
+    for (var i=0; i<arrLen; i++) {
+      var val = arr[i];
+      sum += val;
+    }
+
+    return arrLen <= 0 ? 0 : sum / arrLen;
+  };
+
+  App.prototype._rootMeanSquare = function(arr) {
+    var rms = 0,
+        arrLen = arr.length;
+
+    for (var i=0; i<arrLen; i++) {
+      var val = arr[i];
+      rms += val * val;
+    }
+
+    return Math.sqrt(rms/arrLen);
+  };
+
+  return App;
+
+})();
+
+$(function(){
+  var app = new App();
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+/*
+  to use, run:
+    npm install connect serve-static
+    node server.js
+*/
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+  console.log('Server running on 8080...');
+});
